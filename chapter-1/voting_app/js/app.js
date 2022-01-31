4e0ff01f0aaf79e2676d7045734ac048d8d15b12
skyegrey <dev.skyegrey@gmail.com>
2022-01-31 10:27:45 -0600

    Created first component in app.js

---
+++ chapter-1/voting_app/js/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                Hello, friend! I am a basic react component
+            </div>
+        )
+    }
+}
