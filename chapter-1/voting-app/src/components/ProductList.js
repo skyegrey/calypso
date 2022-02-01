054ec8675035f02ddd9752efe711392ad7a78449
skyegrey <dev.skyegrey@gmail.com>
2022-02-01 13:37:30 -0600

    Created a product list w/ images

--- chapter-1/voting-app/src/components/ProductList.js
+++ chapter-1/voting-app/src/components/ProductList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Product from "../components/Product"
 
 class ProductList extends React.Component {
     render() {
         return (
             <div className='ui unstackable items'>
-                Hello, friend! I am a basic React component
+                <Product />
             </div>
         )
     }
